7fa8613cb4426049ebc9af0d5cdfa06f9af494dc
Anthony Delosa <adelosa@gmail.com>
2021-01-02 10:22:01 +1000

    Ch 13 part 2. ASM includes support

--- asmlg.sh
+++ asmlg.sh
@@ -5,4 +5,4 @@ export REPORT="REPORT.TXT"
 export INVENTRY="COGS.DAT"
 export OFFERSRT="OFFERSRT.DAT"
 export TEACHSRT="TEACHSRT.DAT"
-z390 $1 $2 $3 $4 $5 $6 $7 $8 $9
+z390 $1 "syscpy(+`pwd`/*.mac)" $2 $3 $4 $5 $6 $7 $8 $9
