57efc4ab598e07de03112448db6c818fc6cd5069
Anthony Delosa <adelosa@gmail.com>
2021-01-07 15:58:24 +1000

    Fix binary processing

--- asmlg.sh
+++ asmlg.sh
@@ -3,6 +3,7 @@ export OFFER="OFFER.DAT"
 export TEACHER="TEACHER.DAT"
 export REPORT="REPORT.TXT"
 export INVENTRY="COGS.DAT"
+export INVNTBIN="COGSBIN.DAT"
 export OFFERSRT="OFFERSRT.DAT"
 export TEACHSRT="TEACHSRT.DAT"
 z390 $1 "syscpy(+`pwd`/*.mac)" $2 $3 $4 $5 $6 $7 $8 $9
